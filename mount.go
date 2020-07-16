5d7f1ee1f11683754578bc1aaaf4b3a4165cad75
Conall OCallaghan <cocallaghan@nvidia.com>
2020-07-16 18:18:04 -0400

    fixxxxx

--- mount.go
+++ mount.go
@@ -87,9 +87,13 @@ func Mount(
 	// Serve the connection in the background. When done, set the join status.
 	go func() {
 		server.ServeOps(connection)
-		config.ErrorLogger.Printf("DONE SERVING OPS, CLOSING CONNECTION\n")
+		if config.ErrorLogger != nil {
+			config.ErrorLogger.Printf("DONE SERVING OPS, CLOSING CONNECTION\n")
+		}
 		mfs.joinStatus = connection.close()
-		config.ErrorLogger.Printf("DONE CLOSING CONNECTION.  CLOSING CHANNEL\n")
+		if config.ErrorLogger != nil {
+			config.ErrorLogger.Printf("DONE CLOSING CONNECTION.  CLOSING CHANNEL\n")
+		}
 		close(mfs.joinStatusAvailable)
 	}()
 
